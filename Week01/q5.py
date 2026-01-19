f0b556e11b14d568bf5491221dd442c2eac18612
Aminmamin <mamin.farhanali@gmail.com>
2026-01-18 22:49:48 -0500

    merge q5 and lab1

---
+++ Week01/q5.py
@@ -0,0 +1,22 @@
+userAge = int(input("Enter your age: "))
+
+userAge = userAge + 22
+print("Now showing the shop items filtred by age:", userAge)
+list
+my_array = [1, 4, 7, 9]
+
+# Question 3:
+
+
+# Question 4:
+
+
+# Question 5:
+userAge = int(input("Enter your age: "))
+
+userAge = userAge + 22
+print("Now showing the shop items filtred by age:", userAge)
+
+
+
+
